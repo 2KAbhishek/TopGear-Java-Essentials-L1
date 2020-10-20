0bb883f53926532d092b04c8a7c044fd032c7fef
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-10-20 16:23:27 +0530

    Add Emp.java

---
+++ Tasks/Task-4/Emp.java
@@ -0,0 +1,25 @@
+public class Emp {
+    private String name;
+    private int salary;
+
+    public Emp(String name, int salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSalary() {
+        return this.salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+}
